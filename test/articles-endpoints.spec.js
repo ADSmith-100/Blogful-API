57aca9a98030b23107b477e4d7ec5192d91cf1d0
Adam Smith <coloradoasmith@gmail.com>
2020-09-12 18:14:06 -0600

    some tests working

--- test/articles-endpoints.spec.js
+++ test/articles-endpoints.spec.js
@@ -2,8 +2,9 @@ const { expect } = require("chai");
 const knex = require("knex");
 const app = require("../src/app");
 const { makeArticlesArray } = require("./articles.fixtures");
+const supertest = require("supertest");
 
-describe.only("Articles Endpoints", function () {
+describe("Articles Endpoints", function () {
   let db;
 
   before("make knex instance", () => {
@@ -37,31 +38,160 @@ describe.only("Articles Endpoints", function () {
         return supertest(app).get("/articles").expect(200, testArticles);
       });
     });
-  });
 
-  describe(`GET /articles/:article_id`, () => {
-    context(`Given no articles`, () => {
-      it(`responds with 404`, () => {
-        const articleId = 123456;
+    context(`Given an XSS attack article`, () => {
+      const maliciousArticle = {
+        id: 911,
+        title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+        style: "How-to",
+        content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+      };
+
+      beforeEach("insert malicious article", () => {
+        return db.into("blogful_articles").insert([maliciousArticle]);
+      });
+
+      it("removes XSS attack content", () => {
         return supertest(app)
-          .get(`/articles/${articleId}`)
-          .expect(404, { error: { message: `Article doesn't exist` } });
+          .get(`/articles/${maliciousArticle.id}`)
+          .expect(200)
+          .expect((res) => {
+            expect(res.body.title).to.eql(
+              'Naughty naughty very naughty &lt;script&gt;alert("xss");&lt;/script&gt;'
+            );
+            expect(res.body.content).to.eql(
+              `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`
+            );
+          });
       });
     });
 
-    context("Given there are articles in the database", () => {
-      const testArticles = makeArticlesArray();
-
-      beforeEach("insert articles", () => {
-        return db.into("blogful_articles").insert(testArticles);
+    describe(`GET /articles/:article_id`, () => {
+      context(`Given no articles`, () => {
+        it(`responds with 404`, () => {
+          const articleId = 123456;
+          return supertest(app)
+            .get(`/articles/${articleId}`)
+            .expect(404, { error: { message: `Article doesn't exist` } });
+        });
       });
 
-      it("responds with 200 and the specified article", () => {
-        const articleId = 2;
-        const expectedArticle = testArticles[articleId - 1];
+      context("Given there are articles in the database", () => {
+        const testArticles = makeArticlesArray();
+
+        beforeEach("insert articles", () => {
+          return db.into("blogful_articles").insert(testArticles);
+        });
+
+        it("responds with 200 and the specified article", () => {
+          const articleId = 2;
+          const expectedArticle = testArticles[articleId - 1];
+          return supertest(app)
+            .get(`/articles/${articleId}`)
+            .expect(200, expectedArticle);
+        });
+      });
+
+      context(`Given an XSS attack article`, () => {
+        const maliciousArticle = {
+          id: 911,
+          title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+          style: "How-to",
+          content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+        };
+
+        beforeEach("insert malicious article", () => {
+          return db.into("blogful_articles").insert([maliciousArticle]);
+        });
+
+        it("removes XSS attack content", () => {
+          return supertest(app)
+            .get(`/articles/${maliciousArticle.id}`)
+            .expect(200)
+            .expect((res) => {
+              expect(res.body.title).to.eql(
+                'Naughty naughty very naughty &lt;script&gt;alert("xss");&lt;/script&gt;'
+              );
+              expect(res.body.content).to.eql(
+                `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`
+              );
+            });
+        });
+      });
+    });
+    describe.only(`POST /articles`, () => {
+      it(`creates an article, responding 201 and the new article`, function () {
+        this.retries(3);
+        const newArticle = {
+          title: "Test new article",
+          style: "Listicle",
+          content: "Test new article content...",
+        };
         return supertest(app)
-          .get(`/articles/${articleId}`)
-          .expect(200, expectedArticle);
+          .post("/articles")
+          .send(newArticle)
+          .expect(201)
+          .expect((res) => {
+            expect(res.body.title).to.eql(newArticle.title);
+            expect(res.body.style).to.eql(newArticle.style);
+            expect(res.body.content).to.eql(newArticle.content);
+            expect(res.body).to.have.property("id");
+            expect(res.headers.location).to.eql(`/articles/${res.body.id}`);
+            const expected = new Date().toLocaleString();
+            const actual = new Date(res.body.date_published).toLocaleString();
+            expect(actual).to.eql(expected);
+          })
+          .then((postRes) =>
+            supertest(app)
+              .get(`/articles/${postRes.body.id}`)
+              .expect(postRes.body)
+          );
+      });
+      const requiredFields = ["title", "style", "content"];
+
+      requiredFields.forEach((field) => {
+        const newArticle = {
+          title: "Test new article",
+          style: "Listicle",
+          content: "Test new article content...",
+        };
+
+        it(`responds with 400 and an error message when the '${field}' is missing`, () => {
+          delete newArticle[field];
+
+          return supertest(app)
+            .post("/articles")
+            .send(newArticle)
+            .expect(400, {
+              error: { message: `Missing '${field}' in request body` },
+            });
+        });
+      });
+      context(`Given an XSS attack article`, () => {
+        const maliciousArticle = {
+          id: 911,
+          title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+          style: "How-to",
+          content: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+        };
+
+        beforeEach("insert malicious article", () => {
+          return db.into("blogful_articles").insert([maliciousArticle]);
+        });
+
+        it("removes XSS attack content", () => {
+          return supertest(app)
+            .get(`/articles/${maliciousArticle.id}`)
+            .expect(200)
+            .expect((res) => {
+              expect(res.body.title).to.eql(
+                'Naughty naughty very naughty &lt;script&gt;alert("xss");&lt;/script&gt;'
+              );
+              expect(res.body.content).to.eql(
+                `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`
+              );
+            });
+        });
       });
     });
   });
